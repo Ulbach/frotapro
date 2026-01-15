7d05beb99c14b4fb30e568eacee9c70913600660
Ulbach <124060988+Ulbach@users.noreply.github.com>
2026-01-15 15:03:30 -0400

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -1,11 +1,11 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import App from './App.tsx';
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
-  throw new Error("Não foi possível encontrar o elemento raiz para montar o aplicativo.");
+  throw new Error("Não foi possível encontrar o elemento raiz.");
 }
 
 const root = ReactDOM.createRoot(rootElement);
